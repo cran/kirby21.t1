8116c3c116d72cec90bba3819aa1aadd8c9761cb
John Muschelli <muschellij2@gmail.com>
2017-03-02 07:09:34 +0000

    version 1.5.1

---
+++ R/download_t1_data.R
@@ -0,0 +1,14 @@
+#' @title Download T1 Image Filenames
+#'
+#' @description Download the files for the T1 images
+#' @param ... arguments to pass to \code{\link{download_kirby21_data}},
+#' \code{modalities = "T1"} so it cannot be specified
+#' @return Indicator if the data is downloaded.
+#' 
+#' @export
+#' @importFrom kirby21.base download_kirby21_data all_modalities
+download_t1_data =  function(...) {  
+  x = kirby21.base::download_kirby21_data(modality = "T1", ...)
+  return(x)
+}
+
